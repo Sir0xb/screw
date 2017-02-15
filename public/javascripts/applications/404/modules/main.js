428a042ebad4ebd4bbe9eb69f096e689b969f769
Sir0xb <Sir0xb@live.com>
2017-02-15 22:26:39 +0800

    准备实现小狗奔跑css版本

--- public/javascripts/applications/404/modules/main.js
+++ public/javascripts/applications/404/modules/main.js
@@ -4,9 +4,9 @@ define(["knockout", "Super", "jquery"], function (ko, Super, $) {
     return function (context) {
         var self = Super.call(this, context);
 
-        $("body").one("pageReady", function () {
-            $("body").css("background-color", "#2B303B");
-        });
+        // $("body").one("pageReady", function () {
+        //     $("body").css("background-color", "#2B303B");
+        // });
 
         if (self.data.test) {
             window.main = self;
